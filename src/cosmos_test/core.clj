856afe26094a6369ecaf1f5935a24b1ba54255d7
Yrjö Kari-Koskinen <ykk@peruna.fi>
2019-10-18 23:21:34 +0300

    make simple query to azure cosmos db

--- src/cosmos_test/core.clj
+++ src/cosmos_test/core.clj
@@ -1,7 +1,31 @@
 (ns cosmos-test.core
+  (:require [rx.lang.clojure.interop :as rx])
+  (:import (com.microsoft.azure.cosmosdb ConnectionPolicy ConsistencyLevel FeedOptions)
+           (com.microsoft.azure.cosmosdb.rx AsyncDocumentClient))
   (:gen-class))
 
+(defn- do-query [uri master-key]
+  (let [client (-> (com.microsoft.azure.cosmosdb.rx.AsyncDocumentClient$Builder.)
+                   (.withServiceEndpoint uri)
+                   (.withMasterKey master-key)
+                   (.withConnectionPolicy (ConnectionPolicy/GetDefault))
+                   (.withConsistencyLevel ConsistencyLevel/Session)
+                   .build)
+        query-options (-> (FeedOptions.)
+                          (.setMaxItemCount (int 10)))
+        observable (.queryDocuments
+                     client
+                     "/dbs/ToDoList/colls/Items"
+                     "SELECT * FROM c"
+                     query-options)]
+    (-> observable
+        (.subscribe (rx/action [page]
+                               (->> (-> page .getResults)
+                                    (map #(.toJson %))
+                                    println))
+                    (rx/action [e] (.printStackTrace e))
+                    (rx/action [] (println "Completed"))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [uri master-key]
+  (do-query uri master-key))
