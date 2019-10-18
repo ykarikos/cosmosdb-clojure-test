def731de7daa42665546a56c7a72ed9cbe0a9360
Yrjö Kari-Koskinen <ykk@peruna.fi>
2019-10-18 23:41:59 +0300

    add latch to close connection after observable has been consumed and close client connection

--- src/cosmos_test/core.clj
+++ src/cosmos_test/core.clj
@@ -1,17 +1,12 @@
 (ns cosmos-test.core
   (:require [rx.lang.clojure.interop :as rx])
   (:import (com.microsoft.azure.cosmosdb ConnectionPolicy ConsistencyLevel FeedOptions)
-           (com.microsoft.azure.cosmosdb.rx AsyncDocumentClient))
+           (com.microsoft.azure.cosmosdb.rx AsyncDocumentClient)
+           (java.util.concurrent CountDownLatch))
   (:gen-class))
 
-(defn- do-query [uri master-key]
-  (let [client (-> (com.microsoft.azure.cosmosdb.rx.AsyncDocumentClient$Builder.)
-                   (.withServiceEndpoint uri)
-                   (.withMasterKey master-key)
-                   (.withConnectionPolicy (ConnectionPolicy/GetDefault))
-                   (.withConsistencyLevel ConsistencyLevel/Session)
-                   .build)
-        query-options (-> (FeedOptions.)
+(defn- do-query [client completion-latch]
+  (let [query-options (-> (FeedOptions.)
                           (.setMaxItemCount (int 10)))
         observable (.queryDocuments
                      client
@@ -23,9 +18,23 @@
                                (->> (-> page .getResults)
                                     (map #(.toJson %))
                                     println))
-                    (rx/action [e] (.printStackTrace e))
-                    (rx/action [] (println "Completed"))))))
+                    (rx/action [e]
+                               (.printStackTrace e)
+                               (.countDown completion-latch))
+                    (rx/action []
+                               (println "Completed")
+                               (.countDown completion-latch))))))
 
 (defn -main
   [uri master-key]
-  (do-query uri master-key))
+  (let [query-completion-latch (CountDownLatch. 1)
+        client (-> (com.microsoft.azure.cosmosdb.rx.AsyncDocumentClient$Builder.)
+                   (.withServiceEndpoint uri)
+                   (.withMasterKey master-key)
+                   (.withConnectionPolicy (ConnectionPolicy/GetDefault))
+                   (.withConsistencyLevel ConsistencyLevel/Session)
+                   .build)]
+    (do-query client query-completion-latch)
+    (.await query-completion-latch)
+    (.close client)))
+
